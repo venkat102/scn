c6f004fd9b71b2d94e340b5acdd4fc43cd114d0a
venkat102 <venkatesharunachalam659@gmail.com>
2023-05-06 01:10:06 +0530

    initialize app

---
+++ scn/scn/doctype/invoice_creator/test_invoice_creator.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, venkatesh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInvoiceCreator(FrappeTestCase):
+	pass
